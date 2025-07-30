23c3890e5c5a3d727f474c832d21121f79dc47b2
Vlad Costin <vladcostin58@gmail.com>
2025-07-30 21:13:56 +0200

    Calendario

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -23,6 +23,38 @@ function renderCalendar(date = new Date()) {
     month: "long",
     year: "numeric",
   });
+
+  const weekDays = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
+  weekDays.forEach((day) => {
+    const dayEl = document.createElement("div");
+    dayEl.className = "text-center p-3 h-fit";
+    dayEl.textContent = day.slice(0, 3);
+    calendarEl.appendChild(dayEl);
+  });
+
+  // Crea i div vuoti dove non ci sono giorni
+  for (let i = 0; i < fistDayOfMonth; i++) {
+    calendarEl.appendChild(document.createElement("div"));
+  }
+
+  // Creaiamo il giorno con tutto
+  for (let day = 1; day <= totalDays; day++) {
+    const dateStr = `${year}-${String(month + 1).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+
+    const cell = document.createElement("div");
+    cell.className = "grid place-items-center text-center p-2 rounded-xl shadow-number aspect-square";
+
+    if (day === today.getDate() && month === today.getMonth() && year === today.getFullYear()) {
+      // cell.className = "bg-gradient-to-br from-blue-500 to-blue-600 text-center";
+      cell.className = "grid place-items-center text-center p-2 rounded-xl shadow-number text-white bg-gradient-to-br from-blue-500 to-blue-600 shadow-today text-center aspect-square";
+    }
+
+    const dateEl = document.createElement("div");
+    dateEl.textContent = day;
+    cell.appendChild(dateEl);
+
+    calendarEl.appendChild(cell);
+  }
 }
 
 function changeMonth(offset) {
--- public/index.php
+++ public/index.php
@@ -21,14 +21,14 @@
     </div>
 
     <nav class="flex justify-end gap-3">
-      <button class="px-3 py-2 bg-black border-1 border-slate-200 dark:border-slate-700 rounded-xl">
-        <svg class="w-4 h-4 fill-none stroke-current stroke-1 lucide lucide-bell-icon lucide-bell" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+      <button class="px-3 py-3 bg-white dark:bg-black border-1 border-slate-200 dark:border-slate-700 rounded-xl shadow-sm">
+        <svg class="w-4 h-4 fill-none stroke-current stroke-1 lucide lucide-bell-icon lucide-bell dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
           <path d="M10.268 21a2 2 0 0 0 3.464 0" />
           <path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" />
         </svg>
       </button>
-      <button class="px-3 py-2 bg-black border-1 border-white rounded-xl">
-        <svg class="w-4 h-4 fill-none stroke-current stroke-1 lucide lucide-bell-icon lucide-bell" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+      <button class="px-3 py-3 bg-white dark:bg-black border-1 border-slate-200 dark:border-slate-700 rounded-xl shadow-sm">
+        <svg class="w-4 h-4 fill-none stroke-current stroke-1 lucide lucide-bell-icon lucide-bell dark:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
           <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
           <circle cx="12" cy="12" r="3" />
         </svg>
@@ -41,7 +41,7 @@
   <main class="mx-auto max-w-7xl">
 
     <!-- HTML Corpo del calendario -->
-    <div class="border-1 border-slate-200 dark:border-slate-700 rounded-xl overflow-hidden">
+    <section class="border-1 border-slate-200 dark:border-slate-700 rounded-xl overflow-hidden shadow-sm">
       <!-- HTML contenitore del menu del calendario e del titolo -->
       <div class="bg-gradient-to-r from-white to-slate-50 dark:from-neutral-950 dark:to-zinc-900 p-3 flex flex-col items-center">
         <div class="w-fit min-[426px]:w-full min-[426px]:flex min-[426px]:justify-between">
@@ -61,8 +61,8 @@
           </h3>
           <!-- HTML del menu del calendario -->
           <div class="flex justify-center h-10">
-            <div class="text-xs flex p-1 bg-[#202326] rounded-xl font-medium gap-1">
-              <button class="bg-blue-600 px-3 py-2 rounded-lg flex gap-1.5">
+            <div class="text-xs flex p-1 bg-slate-100 dark:bg-[#202326] rounded-xl font-medium gap-1">
+              <button class="bg-blue-600 px-3 py-2 text-white rounded-lg flex gap-1.5">
                 <span>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-grid3x3-icon lucide-grid-3x3">
                     <rect width="18" height="18" x="3" y="3" rx="2" />
@@ -72,9 +72,9 @@
                     <path d="M15 3v18" />
                   </svg>
                 </span>
-                Month
+                <span class="max-[425px]:hidden">Month</span>
               </button>
-              <button class="px-3 py-2 rounded-lg flex gap-1.5">
+              <button class="px-3 py-2 rounded-lg flex gap-1.5 dark:text-white">
                 <span>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-icon lucide-list">
                     <path d="M3 12h.01" />
@@ -87,7 +87,7 @@
                 </span>
                 Week
               </button>
-              <button class="px-3 py-2 rounded-lg flex gap-1.5">
+              <button class="px-3 py-2 rounded-lg flex gap-1.5 dark:text-white">
                 <span>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-icon lucide-calendar">
                     <path d="M8 2v4" />
@@ -103,14 +103,13 @@
         </div>
         <!-- HTML Cambio mese con frecce -->
         <div class="w-full flex justify-between items-center mt-2">
-          <button id="buttonPrev" class="px-3 py-2 bg-black border-1 border-white rounded-xl">
+          <button id="buttonPrev" class="px-3 py-2 border-slate-200 dark:border-slate-700 dark:bg-black dark:text-white border-1 rounded-xl shadow">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left">
               <path d="m15 18-6-6 6-6" />
             </svg>
           </button>
-          <!-- TODO: Implementare i mesi automatici -->
           <h2 id="monthYear"></h2>
-          <button id="buttonNext" class="px-3 py-2 bg-black border-1 border-white rounded-xl">
+          <button id="buttonNext" class="px-3 py-2 dark:bg-black border-1 border-slate-200 dark:border-slate-700 dark:text-white rounded-xl shadow">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right">
               <path d="m9 18 6-6-6-6" />
             </svg>
@@ -119,10 +118,25 @@
         <!-- TODO: Implementare la barra di ricerca dei eventi -->
       </div>
       <!-- HTML Calendario, il calendario viene generato -->
-      <div id="calendar">
+      <div id="calendar" class="grid grid-cols-7 text-sm font-semibold text-slate-600 dark:text-slate-400 bg-white p-2 gap-2 dark:bg-black border-b-1 border-slate-200">
 
       </div>
-    </div>
+      <div class="flex justify-center items-center bg-white dark:bg-black p-4">
+        <button class="flex justify-center items-center gap-2 w-full py-2.5 px-4 rounded-xl bg-linear-to-r from-[#667eea] to-[#764ba2] text-sm font-semibold text-white">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus">
+              <path d="M5 12h14" />
+              <path d="M12 5v14" />
+            </svg>
+          </span>
+          Aggiungi un evento</button>
+      </div>
+    </section>
+
+    <!-- HTML section Tasks -->
+    <section>
+
+    </section>
   </main>
 </body>
 
